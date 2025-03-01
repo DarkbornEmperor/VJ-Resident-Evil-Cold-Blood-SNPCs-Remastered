5f975f8a2f16b8ede129862d06140c9ff9a33698
Darkborn <darkandrai1987@gmail.com>
2025-03-01 03:19:50 -0500

    Cleanup

--- lua/entities/sent_vj_recb_mapspawner/shared.lua
+++ lua/entities/sent_vj_recb_mapspawner/shared.lua
@@ -1,9 +1,6 @@
 /* Note: All credits go to Cpt. Hazama. I take no credit for this. */
 ENT.Type = "anim"
 ENT.Base = "base_anim"
-ENT.PrintName        = "Zombie Map Spawner"
-ENT.Author            = "Darkborn"
-
-ENT.Spawnable            = false
-ENT.AdminSpawnable        = false
+ENT.PrintName = "Zombie Map Spawner"
+ENT.Author = "Darkborn"
 
